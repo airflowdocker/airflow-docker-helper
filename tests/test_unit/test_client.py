20988302634795dfb4507248aa965af22fc8bae2
Dan Cardin <ddcardin@gmail.com>
2019-04-03 14:14:41 -0400

    Add support for xcom push to the client.

--- tests/test_unit/test_client.py
+++ tests/test_unit/test_client.py
@@ -1,11 +1,12 @@
+import json
+
 from airflow_docker_helper.testing import test_client as get_client
 
 
 class TestBranchToTasks:
-
     def test_one_task_id(self):
         client = get_client()
-        client.branch_to_tasks('test-task')
+        client.branch_to_tasks("test-task")
         open_file = client._mock_branch_to_tasks_file()
         assert len(open_file.write.mock_calls) == 1
         _, args, kwargs = open_file.write.mock_calls[0]
@@ -13,7 +14,7 @@ class TestBranchToTasks:
 
     def test_many_task_ids(self):
         client = get_client()
-        client.branch_to_tasks(['test-task', 'other-task'])
+        client.branch_to_tasks(["test-task", "other-task"])
         open_file = client._mock_branch_to_tasks_file()
         assert len(open_file.write.mock_calls) == 1
         _, args, kwargs = open_file.write.mock_calls[0]
@@ -21,14 +22,13 @@ class TestBranchToTasks:
 
 
 class TestSensor:
-
     def test_true_outcome(self):
         client = get_client()
         client.sensor(True)
         open_file = client._mock_sensor_file()
         assert len(open_file.write.mock_calls) == 1
         _, args, kwargs = open_file.write.mock_calls[0]
-        assert args[0] == b'true'
+        assert args[0] == b"true"
 
     def test_false_outcome(self):
         client = get_client()
@@ -36,24 +36,37 @@ class TestSensor:
         open_file = client._mock_sensor_file()
         assert len(open_file.write.mock_calls) == 1
         _, args, kwargs = open_file.write.mock_calls[0]
-        assert args[0] == b'false'
+        assert args[0] == b"false"
 
 
 class TestShortCircuit:
-
     def test_short_circuit(self):
         client = get_client()
         client.short_circuit()
         open_file = client._mock_short_circuit_file()
         assert len(open_file.write.mock_calls) == 1
         _, args, kwargs = open_file.write.mock_calls[0]
-        assert args[0] == b'false'
+        assert args[0] == b"false"
 
 
 class TestContext:
-
     def test_context(self):
-        mock_context = {'foo': 'bar'}
+        mock_context = {"foo": "bar"}
         client = get_client(mock_context=mock_context)
         context = client.context()
         assert context == mock_context
+
+
+class TestXComPush:
+    def test_write_value(self):
+        client = get_client()
+
+        client.xcom_push(key="foo", value={"bar": "baz"})
+
+        open_file = client._mock_xcom_push_file()
+        assert len(open_file.write.mock_calls) == 1
+        _, args, kwargs = open_file.write.mock_calls[0]
+        assert json.loads(args[0].decode("utf-8")) == {
+            "key": "foo",
+            "value": {"bar": "baz"},
+        }
