f93075e70ff3ac41fa7a725d8f0fd13af401baed
Dan Cardin <ddcardin@gmail.com>
2019-04-03 14:14:41 -0400

    Add support for xcom push to the client.

--- tests/test_unit/test_host.py
+++ tests/test_unit/test_host.py
@@ -3,74 +3,88 @@ from airflow_docker_helper.compat import mock
 
 
 class TestBranchToTasks:
-
     def test_one_task_id(self):
-        client = get_host_client(task_ids=['foo'])
-        tmp_dir = 'META_DIR'
+        client = get_host_client(task_ids=["foo"])
+        tmp_dir = "META_DIR"
         task_ids = client.branch_task_ids(tmp_dir)
-        assert task_ids == ['foo']
+        assert task_ids == ["foo"]
 
     def test_many_task_ids(self):
-        client = get_host_client(task_ids=['foo', 'bar'])
-        tmp_dir = 'META_DIR'
+        client = get_host_client(task_ids=["foo", "bar"])
+        tmp_dir = "META_DIR"
         task_ids = client.branch_task_ids(tmp_dir)
-        assert task_ids == ['foo', 'bar']
+        assert task_ids == ["foo", "bar"]
 
     def test_no_task_ids(self):
         client = get_host_client()
-        tmp_dir = 'META_DIR'
+        tmp_dir = "META_DIR"
         task_ids = client.branch_task_ids(tmp_dir)
         assert task_ids == []
 
 
 class TestSensor:
-
     def test_true_outcome(self):
         client = get_host_client(sensor=True)
-        tmp_dir = 'META_DIR'
+        tmp_dir = "META_DIR"
         sensor_outcome = client.sensor_outcome(tmp_dir)
         assert sensor_outcome is True
 
     def test_false_outcome(self):
         client = get_host_client(sensor=False)
-        tmp_dir = 'META_DIR'
+        tmp_dir = "META_DIR"
         sensor_outcome = client.sensor_outcome(tmp_dir)
         assert sensor_outcome is False
 
     def test_no_outcome(self):
         client = get_host_client()
-        tmp_dir = 'META_DIR'
+        tmp_dir = "META_DIR"
         sensor_outcome = client.sensor_outcome(tmp_dir)
         assert sensor_outcome is False
 
 
 class TestShortCircuit:
-
     def test_short_circuit(self):
         client = get_host_client(short_circuit=False)
-        tmp_dir = 'META_DIR'
+        tmp_dir = "META_DIR"
         short_circuit_outcome = client.short_circuit_outcome(tmp_dir)
         assert short_circuit_outcome is False
 
     def test_no_short_circuit(self):
         client = get_host_client()
-        tmp_dir = 'META_DIR'
+        tmp_dir = "META_DIR"
         short_circuit_outcome = client.short_circuit_outcome(tmp_dir)
         assert short_circuit_outcome is True
 
 
 class TestWriteContext:
-
     def test_context(self):
         client = get_host_client()
-        test_context = {'foo': 'bar'}
-        tmp_dir = 'META_DIR'
-        with mock.patch('airflow_docker_helper.serialize_context', return_value=test_context):
+        test_context = {"foo": "bar"}
+        tmp_dir = "META_DIR"
+        with mock.patch(
+            "airflow_docker_helper.serialize_context", return_value=test_context
+        ):
             client.write_context(test_context, tmp_dir)
             open_file = client._mock_context_file()
             assert len(open_file.write.mock_calls) == 1
             _, args, kwargs = open_file.write.mock_calls[0]
             assert args[0] == b'{"foo": "bar"}'
+
+
+class TestXComPush:
+    def test_write_value(self):
+        client = get_host_client(
+            xcom_push=b'{"key": "foo", "value": [1]}\n{"key": "foo2", "value": [2]}\n'
+        )
+        tmp_dir = "META_DIR"
+        data = client.get_xcom_push_data(tmp_dir)
+        assert data == [{"key": "foo", "value": [1]}, {"key": "foo2", "value": [2]}]
+
+    def test_no_outcome(self):
+        client = get_host_client()
+        tmp_dir = "META_DIR"
+        data = client.get_xcom_push_data(tmp_dir)
+        assert data == []
 
 
 def get_test_context():
@@ -112,10 +126,7 @@ def get_test_context():
         "task_instance_key_str": "adag__task-name__20190222",
         "conf": "blah",
         "test_mode": False,
-        "var": {
-            "value": "None",
-            "json": "None"
-        },
+        "var": {"value": "None", "json": "None"},
         "inlets": [],
-        "outlets": []
+        "outlets": [],
     }
