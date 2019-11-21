51ae2401ba3080c42fdb1ca61bf024ed0610d81d
Hunter Senft-Grupp <huntcsg@gmail.com>
2019-11-21 00:38:39 -0500

    Updated tooling

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     zip_safe=False,
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     extras_require={
+        'dev': parse_requirements('deps/dev-requirements.in'),
         'testing': parse_requirements('deps/testing-requirements.in'),
         'docs': parse_requirements('deps/docs-requirements.in'),
         'linting': parse_requirements('deps/linting-requirements.in'),
